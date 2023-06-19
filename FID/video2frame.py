3e9987b5bce68430f605673095fe0116fff475a9
aolivepe <aolivepe@purdue.edu>
2023-06-18 20:44:19 -0400

    fid

--- FID/video2frame.py
+++ FID/video2frame.py
@@ -1,33 +1,36 @@
 import os
 import imageio
+import argparse
+
 script_path = os.path.dirname(__file__)
 
-# Directorio que contiene los videos
-#directory = os.path.abspath(os.path.join(script_path, '..', '..', '..','..', 'data', 'aolivepe', 'REAL_EXPERIMENTS', 'SUPER_RESOLUTION', 'SRGAN'))
-directory = "/data/aolivepe/newpreprocessedData/psnr_Real-ESRGAN_2"
-# Directorio de salida para guardar las imágenes PNG
-#output_directory = os.path.abspath(os.path.join(script_path, '..', '..', '..','..', 'data', 'aolivepe', 'REAL_EXPERIMENTS', 'SUPER_RESOLUTION', 'SRGAN_frames'))
-output_directory = "/data/aolivepe/newpreprocessedData/psnr_Real-ESRGAN_2"
-# Verificar si el directorio de salida existe, si no, crearlo
+#directory = "/data/aolivepe/newpreprocessedData/psnr_Real-ESRGAN_2"
+#output_directory = "/data/aolivepe/newpreprocessedData/psnr_Real-ESRGAN_2"
+
+parser = argparse.ArgumentParser(description='Example argument parser')
+
+parser.add_argument('--input', help='Input AVI file path')
+parser.add_argument('--output', help='Output PNG file path')
+
+args = parser.parse_args()
+
+directory = args.input
+output_directory = args.output
+
+
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-# Recorrer los archivos en el directorio
 for filename in os.listdir(directory):
     if filename.endswith(".avi"):
-        # Ruta completa del archivo GIF
         gif_path = os.path.join(directory, filename)
 
-        # Leer el archivo GIF
         gif_frames = imageio.mimread(gif_path)
 
-        # Iterar a través de los frames del GIF
         for i, frame in enumerate(gif_frames):
-            # Construir el nombre del archivo de salida
             output_filename = f"{os.path.splitext(filename)[0]}_{i}.png"
 
-            # Ruta completa del archivo de salida
             output_path = os.path.join(output_directory, output_filename)
 
-            # Guardar el frame como imagen PNG
             imageio.imwrite(output_path, frame)
+            
